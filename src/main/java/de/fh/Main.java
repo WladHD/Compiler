5e6c031e86d056c3ccb50e820eb4c47889046c62
Wladislaw Jerokin <wladhd@gmail.com>
2023-09-21 04:26:43 +0200

    doing something

--- src/main/java/de/fh/Main.java
+++ src/main/java/de/fh/Main.java
@@ -3,9 +3,6 @@ package de.fh;
 import de.fh.javacc.generated.ParseException;
 import de.fh.javacc.generated.SimpleNode;
 import de.fh.lexparser.LexParser;
-import de.fh.semantic.SemanticAnalyzer;
-import de.fh.translator.Translator;
-import de.fh.translator.sandbox.TranslatorTemplate;
 
 public class Main {
     private final static boolean standalone = false;
@@ -13,8 +10,8 @@ public class Main {
     public static void main(String[] args) throws ParseException {
         Compiler<SimpleNode> compiler = new Compiler<>(
                 new LexParser(),
-                new SemanticAnalyzer(),
-                new Translator(),
+                null,
+                null,
                 true
         );
 
@@ -23,13 +20,16 @@ public class Main {
             return;
         }
 
-        String source = "arrays.txt";
+        String source = "cleanTest.txt";
         String output = "./src/test/java/TranslatorTemplate.java";
 
         if (args.length > 0) {
             source = args[0];
             output = args.length > 1 ? args[1] : null;
         }
+
+        int[] arr = {1,2,3,4};
+        int x = arr[arr[2] = 1] = 34;
 
         compiler.executePipelineFromResource(source, output);
     }
